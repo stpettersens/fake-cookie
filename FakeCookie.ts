764962223e812fdab25926480302f96a7a3694da
Sam Saint-Pettersen <s.stpettersen+github@gmail.com>
2015-07-04 14:31:33 +0100

    ...

---
+++ FakeCookie.ts
@@ -0,0 +1,51 @@
+/*
+  FakeCookie simulates a browser cookie.
+
+  Copyright 2015 Sam Saint-Pettersen.
+  Released under the MIT License.
+*/
+
+class Cookie {
+
+    private static names = new Array<string>();
+    private static values = new Array<string>();
+
+    // Emulate jquery-cookie method to create or return a cookie.
+    public static cookie(name: string, value?: string, options?: Object): string {
+    	// Options is unused as is only a placeholder for drop-in compatibility 
+    	// with jquery-cookie.
+    	var result: string = null;
+    	var index: number = Cookie.names.indexOf(name);
+    	// RETURN A COOKIE
+        if(value == null) {
+        	// Return cookie value if cookie exists.
+  			if(index != -1) {
+  				result = Cookie.values[index];
+  			}
+  			// Otherwise, will return null.
+        }
+        // CREATE A COOKIE
+        else {
+        	// Overwrite existing cookie:
+        	if(index != -1) {
+        		Cookie.values[index] = value;
+        	}
+        	// Create a new cookie:
+        	else {
+        		Cookie.names.push(name);
+        		Cookie.values.push(value);
+        	}
+        } 
+        return result;
+    }
+
+    // Emulate jquery-cookie method to remove a cookie.
+    public static removeCookie(name: string): void {
+    	var index: number = Cookie.names.indexOf(name);
+    	if(index != -1) {
+    		Cookie.names.splice(index, 1);
+    		Cookie.values.splice(index, 1);
+    	}
+    }
+}
+export = Cookie;
---
+++ Gulpfile.js
@@ -0,0 +1,20 @@
+/*
+	Compile FakeCookie.ts to JavaScript for end-use.
+*/
+
+var gulp = require('gulp'),
+	  fs = require('fs'),
+	 tsc = require('gulp-typescript')
+
+gulp.task('default', function() {
+	return gulp.src('FakeCookie.ts')
+	.pipe(tsc({
+		noImplicitAny: true,
+		module: 'commonjs'
+	}))
+	.pipe(gulp.dest('.'));
+});
+
+gulp.task('clean', function() {
+	fs.unlinkSync('FakeCookie.js');
+});
